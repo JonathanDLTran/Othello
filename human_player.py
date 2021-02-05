fe82c0e73516568ff74c506edcedd20daf7a888c
Jonathan Tran <jtran2718@gmail.com>
2021-02-05 02:42:17 -0800

    add human players

--- human_player.py
+++ human_player.py
@@ -1,26 +1,37 @@
-alpha_dict = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6,
-              "G": 7, 'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, "g": 7}
-inverted_dict = {alpha_dict[key]: key for key in alpha_dict}
+ALPHA_DICT = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, "G": 7, "I": 8,
+              'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, "g": 7, "i": 8}
+INVERTED_DICT = {ALPHA_DICT[key]: key for key in ALPHA_DICT}
+ROWS_LIST = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
 
 def move(game):
-    legal_moves = game.legal_moves
+    legal_moves = game.getPossibleActions()
     while True:
         print(game)
         print("Move Choices: " +
-              ", ".join([inverted_dict[key].upper() for key in sorted(legal_moves)]))
+              ", ".join([str((INVERTED_DICT[pair[1] + 1].upper(), pair[0] + 1)) for pair in sorted(legal_moves, key=lambda pair: pair[1])]))
 
         col = input("Input the column you want to move to: ")
+        row = input("Input the row you want to move to: ")
 
         move = None
-        if col in alpha_dict:
-            move = alpha_dict[col]
+        if col in ALPHA_DICT:
+            col = ALPHA_DICT[col]
         elif col.isdigit():
-            move = int(col)
+            col = int(col)
         else:
             print(f"Invalid Move: {col} is not a valid column. Try again")
             continue
+
+        if row in ROWS_LIST:
+            row = int(row)
+        else:
+            print(f"Invalid Move: {row} is not a valid row. Try again")
+            continue
+
+        move = (row - 1, col - 1)
+
         if move in legal_moves:
             return move
+
         print(f"Invalid Move: {move} is not a valid location. Try again")
-    return move
--- main.py
+++ main.py
@@ -1,1 +1,85 @@
+import time
+
 import othello
+import human_player
+
+
+def game_loop(game, p1_move, p2_move):
+
+    print("Start of game")
+    print(game)
+    num_moves = 0
+    num_rounds = 0
+
+    while True:
+        num_rounds += 1
+
+        print("Player 1 To Move")
+        num_moves += 1
+        move = p1_move(game)
+        game.takeAction(move)
+
+        if game.is_terminal():
+            victory = game.get_reward()
+            if victory > 0:
+                print(f"Player 1 Wins : {victory}")
+            elif victory == 0:
+                print(f"Tie : {victory}")
+            else:
+                print(f"Player 2 Wins : {victory}")
+            print(f"Rounds Played: {num_rounds}")
+            print(f"Moves Made: {num_moves}")
+            print(game)
+            return victory, num_moves
+        print(game)
+
+        print("Player 2 To Move")
+        num_moves += 1
+        move = p2_move(game)
+        game.takeAction(move)
+
+        if game.is_terminal():
+            victory = game.get_reward()
+            if victory > 0:
+                print(f"Player 1 Wins : {victory}")
+            elif victory == 0:
+                print(f"Tie : {victory}")
+            else:
+                print(f"Player 2 Wins : {victory}")
+            print(f"Rounds Played: {num_rounds}")
+            print(f"Moves Made: {num_moves}")
+            print(game)
+            return victory, num_moves
+        print(game)
+
+        time.sleep(0.05)
+
+
+def init_game():
+    game = othello.State()
+    return game
+
+
+def main(p1_move, p2_move):
+    game = init_game()
+    game_loop(game, p1_move, p2_move)
+
+
+def choose_player(player):
+    print(f"Choose a player for {'Player 1' if player else 'Player 2'}: ")
+    print(f"Type 'H' for human, 'Q' to quit")
+    choice = input("Enter your choice: ")
+    choice = choice.strip().lower()
+    if choice == 'h':
+        return human_player.move
+    elif choice == 'q':
+        print("Quitting...")
+        exit(0)
+    else:
+        print("Invalid Choice: Try Again")
+        print()
+        return choose_player(player)
+
+
+if __name__ == "__main__":
+    main(choose_player(True), choose_player(False))
