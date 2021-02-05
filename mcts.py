3fc78a037e26e429dca095d8eb7023f570c4e241
Jonathan Tran <jtran2718@gmail.com>
2021-02-05 13:10:17 -0800

    change mcts

--- mcts.py
+++ mcts.py
@@ -16,14 +16,25 @@ class Node:
         self.visits = 0
         self.parent = None
 
+    def add_child(self):
+        pass
+
     def ucb(self):
         return
 
 
 class MCTS:
-    def __init__(self, trials):
-        self.tree = Node(True)
-        self.trials = trials
+    def __init__(self, state, num_trials):
+        self.tree = Node(state.getCurrentPlayer(), state)
+        self.trials = num_trials
+
+    def search(self):
+
+        for _ in range(self.trials):
+            self.selection()
+            self.expansion()
+            self.simulation()
+            self.backpropagation()
 
     def selection(self):
         pass
@@ -34,7 +45,21 @@ class MCTS:
     def simulation(self, node):
         assert type(node) == Node
         assert not node.state.isTerminal()
-        actions = node.getPossibleActions()
+
+        actions = node.state.getPossibleActions()
+        rewards = 0
+        for action in actions:
+            childstate = node.state.takeAction(action)
+            reward = rollout(childstate)
+            rewards += reward
 
     def backpropagation(self):
         pass
+
+
+def rollout(state, choice_func=random.choice):
+    while not state.isTerminal():
+        actions = state.getPossibleActions()
+        action = choice_func(actions)
+        state = state.takeAction(action)
+    return state.getReward()
